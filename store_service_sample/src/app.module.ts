e1867be1f7649a8d39f424d8fcde20a6dec9350b
dhkehd2 <dhkehd2@gmail.com>
2023-06-12 20:14:20 +0900

    logger interceptor 추가

--- store_service_sample/src/app.module.ts
+++ store_service_sample/src/app.module.ts
@@ -3,12 +3,20 @@ import { StoreModule } from './apis/store/store.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SharedModule } from './shared/shared.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggingInterceptor } from './interceptros/logging.interceptor';
 
 const apiModules = [StoreModule];
 
 @Module({
   imports: [SharedModule, ...apiModules],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
